2bb87f5babd9240b898132ef2cc912699240c2db
Ratna Srivastava <srivastavaratna1611@gmail.com>
2025-05-02 15:20:39 +0530

    initial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("org.json:json:20210307")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.android.material:material:1.11.0")
 
     implementation("com.google.mlkit:text-recognition:16.0.0-beta3")
 }
